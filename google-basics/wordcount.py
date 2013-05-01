65f772d87184abbce93a539d1bbf5f3c264a4b54
Pavel Bashmakov <pavel.bashmakov@gmail.com>
2013-05-01 16:17:12 +0300

    Implement word count and word top functions

--- google-basics/wordcount.py
+++ google-basics/wordcount.py
@@ -38,6 +38,7 @@ print_words() and print_top().
 """
 
 import sys
+import re
 
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
@@ -45,18 +46,32 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
-def print_words(filename):
-
-  print 'Print words with filename = ' + filename
-
+def find_words_with_counts(filename):
+  dict = {}
   f = open(filename, 'r')
   for line in f:
-    print line,
+    words = re.findall(r'\w+', line)
+    for word in words:
+      word = word.lower()
+      if word in dict:
+        dict[word] += 1
+      else:
+        dict[word] = 1
+  f.close()
+  return dict
+
+def print_words(filename):
+  dict = find_words_with_counts(filename)
+  for word in sorted(dict.keys()):
+    print word, dict[word]
 
 
 # Prints top word in a file
 def print_top(filename):
-  print 'Print top with filename = ' + filename
+  dict = find_words_with_counts(filename)
+  for word in sorted(dict.keys(), key=lambda key: -dict[key]):
+    print word + ' ' + str(dict[word])
+  
 
 ###
 
